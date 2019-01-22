92d7ff5ca621a0b772995d2bf099dcd5c457cf48
Sebastian Łuc <koemirai@gmail.com>
2019-01-22 23:36:06 +0100

    Update main.cpp

--- Neuron/main.cpp
+++ Neuron/main.cpp
@@ -3,10 +3,11 @@
 #include <iostream>
 
 int main(void){
+    char nazwa = 's';
+    double val = -0.1;
     // Sigmoid function A:
     printf("Sigmoid(x): \n");
-    char nazwa = 's';
-    Neuron *n1 = new Neuron(-0.1, nazwa);
+    Neuron *n1 = new Neuron(val, nazwa);
     std::cout<<"Val Neuron: "<<n1->getVal() << std::endl;
     std::cout<<"Activated Val Neuron: "<< n1->getActivatedVal() <<std::endl;
     std::cout<<"Derivated Val Neuron: "<<n1->getDerivedVal() << std::endl;
@@ -14,7 +15,7 @@ int main(void){
 
     printf("Tanh(x) \n");
     nazwa = 't';
-    Neuron *n2 = new Neuron(-0.1, nazwa);
+    Neuron *n2 = new Neuron(val, nazwa);
     std::cout<<"Val Neuron: "<<n2->getVal() << std::endl;
     std::cout<<"Activated Val Neuron: "<< n2->getActivatedVal() <<std::endl;
     std::cout<<"Derivated Val Neuron: "<<n2->getDerivedVal() << std::endl;
@@ -22,7 +23,7 @@ int main(void){
 
     printf("Relu: \n");
     nazwa = 'r';
-    Neuron *n3 = new Neuron(-0.1, nazwa);
+    Neuron *n3 = new Neuron(val, nazwa);
     std::cout<<"Val Neuron: "<<n3->getVal() << std::endl;
     std::cout<<"Activated Val Neuron: "<< n3->getActivatedVal() <<std::endl;
     std::cout<<"Derivated Val Neuron: "<<n3->getDerivedVal() << std::endl;
